e697d80be813d1c8f4ba11bce0929dd7409b7375
charleston10 <charleston.anjos@gmail.com>
2020-07-29 09:55:35 -0300

    create items only time

--- application/app/src/main/java/com/charleston/espressocustomassertion/MainActivity.kt
+++ application/app/src/main/java/com/charleston/espressocustomassertion/MainActivity.kt
@@ -11,6 +11,19 @@ class MainActivity : AppCompatActivity(R.layout.activity_main),
 
     private val listAdapter by lazy { ListAdapter() }
 
+    private val items = listOf(
+        ItemModel("Herrera", 10),
+        ItemModel("Young", 20),
+        ItemModel("Watson", 30),
+        ItemModel("Woods", 40),
+        ItemModel("Berry", 50),
+        ItemModel("Carter", 60),
+        ItemModel("Mcdonald", 70),
+        ItemModel("Payne", 80),
+        ItemModel("Dunn", 90),
+        ItemModel("Stone", 100)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,19 +42,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main),
     }
 
     private fun getList(filter: Boolean = false): List<ItemModel> {
-        val items = listOf(
-            ItemModel("Herrera", 10),
-            ItemModel("Young", 20),
-            ItemModel("Watson", 30),
-            ItemModel("Woods", 40),
-            ItemModel("Berry", 50),
-            ItemModel("Carter", 60),
-            ItemModel("Mcdonald", 70),
-            ItemModel("Payne", 80),
-            ItemModel("Dunn", 90),
-            ItemModel("Stone", 100)
-        )
-
         return if (filter) {
             items.filter { it.age <= 50 }
         } else {
